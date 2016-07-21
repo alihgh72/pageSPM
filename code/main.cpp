2c6da03201680e832bddfefa869d5f5cb625377e
ali <alihgh72@gmail.com>
2016-07-21 14:16:38 +0430

    unconditional branch has problem & branch destinations arnt first instruction

--- code/main.cpp
+++ code/main.cpp
@@ -16,8 +16,8 @@ void initial();
 unsigned int decodeInputIns(string tmp);
 unsigned int decodeInputBranch(string tmp);
 void readAssembly();
-void createBasicBlock();
-void createBlocks(int);
+void createBasicBlock(int);
+
 class BB
 {
 public:
@@ -54,31 +54,31 @@ public:
 unsigned int addrIns[ADDR_SPACE];
 unsigned int targetAddr[ADDR_SPACE];
 BB* startBlock[ADDR_SPACE];
-int block_size = 40;
+int block_size = 10;
 unsigned int minAddr = 0;
 unsigned int maxAddr = 0;
+
 
 int main()
 {
 	initial();
 	readAssembly();
-	createBasicBlock();
-	createBlocks(block_size);
+	createBasicBlock(block_size);
 
 	cout << "codesize = " << maxAddr - minAddr << "Byte \n";
 	int i = 0;
 	int cntr = 1;
 	while (i < ADDR_SPACE && addrIns[i] != 0)
 	{
-		if (startBlock[i] != NULL)
+		if (i == 0 || startBlock[i] != startBlock[i-1])
 		{
+			//cout << cntr << "\t" << startBlock[i] << endl;
 			cout << cntr << "\t" << startBlock[i]->start << "\t" << startBlock[i]->size << "\t" << startBlock[i]->next1<< "\t" << startBlock[i]->end << endl;
 			cntr++;
 		}
 		i++;
 	}
-	//for (int i = 0; i < 100; i++)
-		//cout << i << "\t" << addrIns[i] << endl;
+	
 	getchar();
 	return 0;
 }
@@ -91,7 +91,6 @@ void initial()
 		targetAddr[i] = 0;
 		startBlock[i] = NULL;
 	}
-	block_size = 40;
 	minAddr = 0;
 	maxAddr = 0;
 
@@ -99,6 +98,11 @@ void initial()
 
 void readAssembly()
 {
+	// TODO : read trace file also and 
+	// find branch instruction 
+	// and instruction like pc = rl
+	// for return or CALL
+
 	//loc = location + inputFilename;
 	string loc = "input1.txt";
 	ifstream fin;
@@ -160,58 +164,67 @@ void readAssembly()
 	return;
 }
 
-void createBasicBlock()
+void createBasicBlock(int block_size)
 {
 	int ins_cnt_total = (maxAddr - minAddr) / 4;
 
 	startBlock[0] = new BB();
 	startBlock[0]->start = addrIns[0];
+	BB* curr = startBlock[0];
 	cout << "new block at address " << addrIns[0] << endl;
 
-	for (int i = 0; i <= ins_cnt_total; i++)
+	while (1)
 	{
-		if (targetAddr[i] != 0)
+		curr->size = block_size;
+		int branch = 0;
+		for (unsigned int i = 0; i < curr->size; i++)
 		{
-			int t = (targetAddr[i] - minAddr) / 4;
-			if (startBlock[t] == NULL)
+			int x = (curr->start - minAddr) / 4 + i;
+			startBlock[x] = curr;
+			if ((targetAddr[x] != 0) && ((targetAddr[x] > curr->start + 4 * curr->size) || (targetAddr[x] < curr->start)))
 			{
-				startBlock[t] = new BB();
-				startBlock[t]->start = targetAddr[i];
-				cout << "new block at address " << targetAddr[i] << endl;
+				branch++;
+				if (branch > 1)
+				{
+					curr->size = i - 1;
+					i = 0;
+					branch = 0;
+					cout << curr->start << " shrink block size " << curr->size << endl;
+				}
 			}
 		}
-	}
-}
+		curr->end = curr->start + curr->size * 4;
 
-void createRealBasicBlock()
-{
-	int ins_cnt_total = (maxAddr - minAddr) / 4;
-
-	startBlock[0] = new BB();
-	startBlock[0]->start = addrIns[0];
-	cout << "new block at address " << addrIns[0] << endl;
-
-	for (int i = 0; i <= ins_cnt_total; i++)
-	{
-		if (targetAddr[i] != 0)
+		if (curr->end > maxAddr)
 		{
-			int t = (targetAddr[i] - minAddr) / 4;
-			if (startBlock[t] == NULL)
-			{
-				startBlock[t] = new BB();
-				startBlock[t]->start = targetAddr[i];
-				cout << "new block at address " << targetAddr[i] << endl;
-			}
+			curr->end = maxAddr;
+			curr->size = (curr->end - curr->start) / 4;
+			break;
+		}
 
-			t = (addrIns[i + 1] - minAddr) / 4;
-			if (startBlock[t] == NULL)
-			{
-				startBlock[t] = new BB();
-				startBlock[t]->start = addrIns[i + 1];
-				cout << "new block at address " << addrIns[i + 1] << endl;
-			}
+		int t = (curr->end - minAddr) / 4;
+			startBlock[t] = new BB();
+			startBlock[t]->start = addrIns[t];
+			cout << "new block at address " << addrIns[t] << endl;
+		curr->next2 = startBlock[t];
+		curr = startBlock[t];
+
+	}
+
+	for (int i = 0; i < ins_cnt_total; i++)
+	{
+		int debugCntr = 0;
+		unsigned int tar = (targetAddr[i] - minAddr) / 4;
+		if (targetAddr[i] != 0 && startBlock[i] != startBlock[tar])
+		{
+			debugCntr++;
+			if (debugCntr > 1)
+				cout << "DEBUG_ERR: Basic Block " << startBlock[i]->start << " has more than one branch!!\n";
+			startBlock[i]->next1 = startBlock[tar];
 		}
 	}
+
+
 }
 
 unsigned int decodeInputIns(string tmp)
@@ -238,65 +251,4 @@ unsigned int decodeInputBranch(string tmp)
 		ss >> x;
 	}
 	return x;
-}
-void createBlocks(int block_size)
-{
-
-	int ins_cnt_total = (maxAddr - minAddr) / 4;
-	BB* curr = startBlock[0];
-
-
-	while (1)
-	{
-		curr->size = block_size;
-		for (int i = 0; i < curr->size; i++)
-		{
-			int x = (curr->start - minAddr) / 4 + i;
-			if (startBlock[x] != NULL && i != 0)
-			{
-				curr->next1 = NULL;
-				curr->size = i - 1;
-				i = 0;
-				cout << curr->start << " shrink block size " << curr->size << endl;
-			}
-			else if ((targetAddr[x] != 0) && ((targetAddr[x] > curr->start + 4 * curr->size) || (targetAddr[x] < curr->start)))
-			{
-				int t = (targetAddr[x] - minAddr) / 4;
-				if (curr->next1 == NULL)
-				{
-					if (startBlock[t] == NULL)
-					{
-						cout << "ERROR: there is target that is NULL\n";
-						getchar();
-						return;
-					}
-					curr->next1 = startBlock[t];
-					cout << curr->start << "next1 is " << curr->next1->start << endl;
-				}
-				else
-				{
-					//TODO if its unconditional branch its ok
-					curr->next1 = NULL;
-					curr->size = i - 1;
-					i = 0;
-					cout << curr->start << " shrink block size " << curr->size << endl;
-				}
-			}
-		}
-		curr->end = curr->start + curr->size * 4;
-		if (curr->end > maxAddr)
-		{
-			curr->end = maxAddr;
-			curr->size = (curr->end - curr->start)/4;
-			break;
-		}
-		int t = (curr->end - minAddr) / 4 + 1;
-		if (startBlock[t] == NULL)
-		{
-			startBlock[t] = new BB();
-			startBlock[t]->start = addrIns[t];
-			cout << "new block at address " << addrIns[t] << endl;
-		}
-		curr = startBlock[t];
-	}
 }
